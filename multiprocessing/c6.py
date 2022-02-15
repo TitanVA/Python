a835631410ac8834bd85ae74749f41df8cc192c4
Viktor Bezai <32422942+TitanVA@users.noreply.github.com>
2022-02-15 14:49:19 +0300

    Educate docker

---
+++ multiprocessing/c6.py
@@ -0,0 +1,23 @@
+import time
+from multiprocessing import Process
+import os
+
+CNT = 0
+
+
+def f():
+    global CNT
+    print(f"Started from", os.getpid())
+    time.sleep(3)
+    CNT += 2
+    print(f"f: {CNT} from", os.getpid())
+
+
+if __name__ == '__main__':
+    p = Process(target=f, args=())
+    p_1 = Process(target=f, args=())
+    p_2 = Process(target=f, args=())
+    p.start()
+    p_1.start()
+    p_2.start()
+    print("Finished")
